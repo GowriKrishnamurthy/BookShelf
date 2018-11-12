d9f1b392c6b45dda626b133c7118ff623d7d52f3
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-11-12 18:47:19 +1100

    book model

---
+++ src/app/books/models/book.model.ts
@@ -0,0 +1,13 @@
+export interface IBook {
+    bookID: number;
+    bookTitle: string;
+    bookCode: string;
+    author:string;
+    PublicationDate: string;
+    price: number;
+    description: string;
+    starRating:number,
+    imgUrl: string;
+  }
+  
+  
