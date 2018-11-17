74e62e31e9bb9b8ef88543ce7f45ab23d734102c
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-11-17 19:58:01 +1100

    @Input variable impl to share data between nested and container component

--- src/app/shared/star/star.component.ts
+++ src/app/shared/star/star.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'bookshelf-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.css']
 })
-export class StarComponent implements OnInit  {
+export class StarComponent implements OnInit, OnChanges {
+  @Input() rating = 0;
+  starWidth = 0;
  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(): void {
+    this.starWidth = this.rating * 75 / 5;
   }
 
  
