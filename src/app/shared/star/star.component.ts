ad111e422e461837b40520f1fd49a1e159182377
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-11-17 20:00:16 +1100

    @output - emit event to container content

--- src/app/shared/star/star.component.ts
+++ src/app/shared/star/star.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angu
 export class StarComponent implements OnInit, OnChanges {
   @Input() rating = 0;
   starWidth = 0;
- 
+  @Output() ratingClicked: EventEmitter<string> =
+    new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
@@ -18,5 +20,9 @@ export class StarComponent implements OnInit, OnChanges {
     this.starWidth = this.rating * 75 / 5;
   }
 
- 
+  onClick(): void {
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
+  }
+
+
 }
