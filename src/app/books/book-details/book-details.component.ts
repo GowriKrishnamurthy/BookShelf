16d70766565561c11d9b9b1748b561847b95ba46
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-11-19 19:32:32 +1100

    removed selector as we are not going to nest this component in any component

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-book-details',
+  // No need of selector as we are not going to nest this component in any component
+  // we'll instead display the component's view as part of the routing, 
+
+  /* selector: 'bookshelf-bookdetails', */
+
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.css']
 })
