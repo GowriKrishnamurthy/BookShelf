b61e9ba9ceef6fc4f48a349b3b100028f487eb45
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-11-19 18:36:41 +1100

    error handlers on observables

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -16,6 +16,24 @@ export class BookService {
 
   public getBooks(): Observable<IBook[]> {
     return this.httpClient.get<IBook[]>(this.booksURL).pipe(
-      tap(data => console.log('All books:' + JSON.stringify(data))));
+      tap(data => console.log('All books:' + JSON.stringify(data))),
+      catchError(this.handleErrors)
+    );
+  }
+
+  private handleErrors(err: HttpErrorResponse) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
   }
 }
