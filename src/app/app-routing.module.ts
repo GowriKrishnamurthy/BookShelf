c9da22913f97a6410ca3d34139b0d6702822bb14
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-11-25 16:01:01 +1100

    Angular router module for app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { ErrorComponent } from './shared/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'welcome', component: WelcomeComponent },
+    { path: 'error', component: ErrorComponent },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome/welcome.component';
-import { RouterModule } from '@angular/router';
-import { ErrorComponent } from './shared/error/error.component';
 import { BookModule } from './books/book.module';
 import { SharedModule } from './shared/shared.module';
 
@@ -17,14 +15,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path: 'welcome', component: WelcomeComponent },
-      { path: 'error', component: ErrorComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ]),
-    AppRoutingModule,
     BookModule,
+    AppRoutingModule,
     SharedModule
   ],
   providers: [],
